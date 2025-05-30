35edf22ec308c454314eba56e0d9f9d0dda8cd4a
Ali Eldor <alieldor02@gmail.com>
2025-05-30 08:14:39 +0300

    add router setup with authentication guards

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,44 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { authService } from "../services/axios";
 
+const routes = [
+  {
+    path: "/",
+    redirect: "/login",
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
+    meta: { requiresGuest: true },
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: () => import("../views/Home.vue"),
+    meta: { requiresAuth: true },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = authService.isAuthenticated();
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next("/login");
+    return;
+  }
+
+  if (to.meta.requiresGuest && isAuthenticated) {
+    next("/home");
+    return;
+  }
+
+  next();
+});
 
 export default router;
