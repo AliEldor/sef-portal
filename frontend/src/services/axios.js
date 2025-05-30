e1d706771b3b8219fb6e9616657f0cb431131750
Ali Eldor <alieldor02@gmail.com>
2025-05-30 07:43:08 +0300

    add mainAPI axios instance

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -9,3 +9,12 @@ const randomUserAPI = axios.create({
   },
 });
 
+const mainAPI = axios.create({
+  baseURL: "http://localhost:4000/api", //
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
