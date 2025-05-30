9aa7fa956d15eca2cb220af4fac36a2210304a3d
Ali Eldor <alieldor02@gmail.com>
2025-05-30 07:38:17 +0300

    add database connection

--- backend/server.js
+++ backend/server.js
@@ -18,3 +18,14 @@ app.use(
   })
 );
 
+// Database connection
+export const db = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "se_factory_portal",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
