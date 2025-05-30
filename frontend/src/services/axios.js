bf50f7f16174ce8f3ad0c541b18607b75b15fbbd
Ali Eldor <alieldor02@gmail.com>
2025-05-30 07:42:36 +0300

    add axios instance for random user api

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,3 +1,11 @@
 import axios from "axios";
 
+// create axios instance
+const randomUserAPI = axios.create({
+  baseURL: "https://randomuser.me/api/",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
