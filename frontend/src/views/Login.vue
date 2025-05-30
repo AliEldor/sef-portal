4297c577036d2bf6e5615ba5e340a5ab6a09835e
Ali Eldor <alieldor02@gmail.com>
2025-05-30 07:57:38 +0300

    add basic login view structure

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login-container">
+    <div class="login-card">
+      <!-- Header Section -->
+      <div class="login-header">
+        <div class="logo">
+          <Layers :size="60" stroke="white" />
+        </div>
+        <h1>SE Factory Portal</h1>
+        <p>Access your student dashboard</p>
+      </div>
+
+      
+    </div>
+
+   
+  </div>
+</template>
+
