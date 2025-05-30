d803eec4613268163c98dfb3e2b877444e369121
Ali Eldor <alieldor02@gmail.com>
2025-05-30 07:58:14 +0300

    add login form

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -10,7 +10,64 @@
         <p>Access your student dashboard</p>
       </div>
 
-      
+      <!-- Login Form -->
+      <form @submit.prevent="handleLogin" class="login-form">
+        <div class="form-group">
+          <label for="email">Email Address</label>
+          <div class="input-wrapper">
+            <Mail class="input-icon" :size="28" />
+            <input
+              type="email"
+              id="email"
+              v-model="email"
+              required
+              placeholder="Enter your email"
+              :class="{ error: emailError }"
+            />
+          </div>
+          <span v-if="emailError" class="error-message">{{ emailError }}</span>
+        </div>
+
+        <div class="form-group">
+          <label for="password">Password</label>
+          <div class="input-wrapper">
+            <Lock class="input-icon" :size="28" />
+            <input
+              :type="showPassword ? 'text' : 'password'"
+              id="password"
+              v-model="password"
+              required
+              placeholder="Enter your password"
+              :class="{ error: passwordError }"
+            />
+            <button
+              type="button"
+              @click="togglePassword"
+              class="password-toggle"
+            >
+              <Eye v-if="!showPassword" :size="28" />
+              <EyeOff v-else :size="28" />
+            </button>
+          </div>
+          <span v-if="passwordError" class="error-message">{{ passwordError }}</span>
+        </div>
+
+        <div class="form-options">
+          <label class="remember-me">
+            <input type="checkbox" v-model="rememberMe" />
+            <span class="checkmark"></span>
+            Remember me
+          </label>
+          <a href="#" class="forgot-password">Forgot Password?</a>
+        </div>
+
+        <button type="submit" class="login-button" :disabled="isLoading">
+          <span v-if="!isLoading">Sign In</span>
+          <div v-else class="loading-spinner"></div>
+        </button>
+      </form>
+
+     
     </div>
 
    
